e1ff4b7e9b55832a1460da2de0a28119b9d0751a
OgwoGP <digispowerllc@gmail.com>
2025-08-14 15:39:48 +0100

    Update AuthForm.tsx

--- src/app/agent/signin/AuthForm.tsx
+++ src/app/agent/signin/AuthForm.tsx
@@ -260,25 +260,9 @@ export const AuthForm = ({
             </>
           )}
 
-          {/* Remember Me + Forgot Password */}
+          {/* Forgot Password */}
           <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                checked={rememberMe}
-                onChange={(e) => setRememberMe(e.target.checked)}
-                className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded"
-                disabled={submitting}
-              />
-              <label
-                htmlFor="remember-me"
-                className="ml-2 block text-sm text-green-700 hover:text-green-600"
-              >
-                Remember me
-              </label>
-            </div>
+            <div className="flex items-center"></div>
             <div className="text-sm">
               <Link
                 href="/agent/forgot-password"
